820b0146d1035f61133047d7b5cbaadb7c842adf
Rutvik Kshirsagar <rutvikkshirsagar188@gmail.com>
2023-03-01 21:03:46 +0530

    Find pairs in Array

---
+++ 14. Arrays/pair_of_array.java
@@ -0,0 +1,18 @@
+// Find pairs in array
+
+public class pair_of_array {
+    public static void pairs_in_array(int arr[]){
+        for(int i = 0; i<arr.length; i++){
+            for(int j = i+1; j<arr.length; j++){
+                System.out.print("("+arr[i]+","+arr[j]+")");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {2,4,6,8,10};
+
+        pairs_in_array(arr);
+        
+    }
+}
