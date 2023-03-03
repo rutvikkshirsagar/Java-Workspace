3263d7da511591f047fd246f715a4a1da2a90ada
Rutvik Kshirsagar <rutvikkshirsagar188@gmail.com>
2023-03-04 03:20:11 +0530

    Calculating maximum sum of subarray O(n3) comp

---
+++ 14. Arrays/maxSubarraySum.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class maxSubarraySum {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number of element in array : ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+        System.out.print("Enter "+ n+" elements : ");
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max_sum = 0;
+        int sum = 0;
+        for(int i = 0; i<n; i++){
+            for(int j = i+1; j<n; j++){
+                for(int k = i; k<=j; k++){
+                    sum=sum + arr[k];
+                    //System.out.println(sum);
+                }
+                if(sum>max_sum){
+                    max_sum = sum;
+                }
+                sum = 0;
+                //System.out.println();
+            }
+            //System.out.println();
+        }
+        System.out.println(max_sum);
+    }
+}
