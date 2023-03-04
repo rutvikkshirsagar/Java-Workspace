3e613ff38bc3e2e969fc48f9e49cf284d79185b7
Rutvik Kshirsagar <rutvikkshirsagar188@gmail.com>
2023-03-05 00:21:21 +0530

    Sorting using bubble sort

--- 15. Basic Sorting Algorithm/bubble_sort.java
+++ 15. Basic Sorting Algorithm/bubble_sort.java
@@ -4,7 +4,7 @@ public class bubble_sort {
     public static void bubbleSort(int arr[]){
         for(int i = 0; i<arr.length; i++){
             for(int j = 0; j<arr.length-i-1; j++){
-                if(arr[j+1]<arr[j]){
+                if(arr[j]>arr[j+1]){
                     int temp = arr[j+1];
                     arr[j+1] = arr[j];
                     arr[j] = temp;
