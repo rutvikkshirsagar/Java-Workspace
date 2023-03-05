3e3590d59ed49fb8e4e7c48e1dc4d5f49de6fa2d
Rutvik Kshirsagar <rutvikkshirsagar188@gmail.com>
2023-03-05 19:54:37 +0530

    Sorting array using selection sort in O(n2)

---
+++ 15. Basic Sorting Algorithm/insertion_sort.java
@@ -0,0 +1,30 @@
+// Sorting array using selection sort in O(n2)
+import java.util.*;
+public class insertion_sort {
+    public static void insertionSort(int arr[]){
+        for(int i = 1; i<arr.length; i++){
+            int curr = arr[i];
+            int prev = i-1;
+            while(prev>=0 && arr[prev]>curr){
+                arr[prev+1] = arr[prev];
+                prev--;
+            }
+            arr[prev+1] = curr;
+        }
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of array : ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        System.out.print("Enter elements of array : ");
+        for(int i = 0; i<n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.print("Sorted array will look like :");
+        insertionSort(arr);
+        for(int i = 0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+}
