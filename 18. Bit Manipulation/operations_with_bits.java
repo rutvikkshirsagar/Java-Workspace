0dbcd0cc1fb01ebbe371ef62395a9b9d0839e397
Rutvik Kshirsagar <rutvikkshirsagar188@gmail.com>
2023-03-13 13:28:34 +0530

    Operations with bits

--- 18. Bit Manipulation/operations_with_bits.java
+++ 18. Bit Manipulation/operations_with_bits.java
@@ -1,3 +1,4 @@
+// Operations with bits (get bit, set bit, clear bit)
 public class operations_with_bits {
     public static int getIthBit(int n, int i){
         if((n & (1<<i))==0){
@@ -14,9 +15,19 @@ public class operations_with_bits {
     public static int clearIthBit(int n, int i){
         return n & (~(1<<i));
     }
+    // upadate ith bit with given bit
+    public static int updateIthBit(int n, int i, int b){
+        if(b==1){
+            return setIthBit(n, i);
+        }
+        else{
+            return clearIthBit(n, i);
+        }
+    }
     public static void main(String args[]){
         System.out.println(getIthBit(10, 3));
         System.out.println(setIthBit(10, 2));
         System.out.println(clearIthBit(10, 1));
+        System.out.println(updateIthBit(10, 2, 1));
     }
 }
